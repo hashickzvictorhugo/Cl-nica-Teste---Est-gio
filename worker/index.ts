a96809abc0b0c09193ff2855d4a1748086a15b69
Victor Hugo Durães Alves <vduraesalves1122@gmail.com>
2026-09-15 14:23:43 -0300

    security: aplica headers no entrypoint do Worker

---
+++ worker/index.ts
@@ -0,0 +1,14 @@
+import handler from "vinext/server/fetch-handler";
+
+import { applySecurityHeaders } from "../lib/security-response";
+
+export default {
+  async fetch(
+    request: Request,
+    env: Cloudflare.Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    const response = await handler.fetch(request, env, ctx);
+    return applySecurityHeaders(response, new URL(request.url).pathname);
+  },
+};
